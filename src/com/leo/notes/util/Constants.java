b0e24225c446b3f5028f9f0846ac2eee13e2e5c6
shichunlei <1558053958@qq.com>
2015-07-15 12:50:49 +0800

    修改Bomb Application Id

--- src/com/leo/notes/util/Constants.java
+++ src/com/leo/notes/util/Constants.java
@@ -2,5 +2,5 @@ package com.leo.notes.util;
 
 public class Constants {
 
-	public static String APPLICATION_ID = "e4fdd99da905595c6669b3d0b9aa4c89";
+	public static String APPLICATION_ID = "ac40dc2b1d73ba5f2957eb43f095d3b9";
 }
