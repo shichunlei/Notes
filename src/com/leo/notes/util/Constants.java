0b7c1471fa155586cf2809369593b44e8ff49bfe
shichunlei <1558053958@qq.com>
2015-07-16 10:10:06 +0800

    添加全局常量

--- src/com/leo/notes/util/Constants.java
+++ src/com/leo/notes/util/Constants.java
@@ -1,6 +1,46 @@
 package com.leo.notes.util;
 
+import scl.leo.library.utils.other.TimeUtils;
+import android.os.Environment;
+
 public class Constants {
 
+	/** Bomb APIKEY */
 	public static String APPLICATION_ID = "ac40dc2b1d73ba5f2957eb43f095d3b9";
+
+	/** SDcard根目录 */
+	public static final String SDCARD_PATH = Environment
+			.getExternalStorageDirectory().getAbsolutePath();
+
+	/** 根文件夹 */
+	public static final String SCLUI_FOLDER_NAME = "/NOTES";
+	/** 根文件夹路径 */
+	public static final String SCLUI_FOLDER_PATH = SDCARD_PATH + "/NOTES/";
+
+	/** 下载的图片的文件夹 */
+	public static final String DOWNLOAD_FOLDER_NAME = "/download/";
+	/** 下载的图片的文件夹路径 */
+	public static final String DOWNLOAD_FOLDER_PATH = SCLUI_FOLDER_PATH
+			+ DOWNLOAD_FOLDER_NAME;
+
+	/** 拍照临时图片名称 */
+	public static final String TEMPORARY_IMAGE_NAME = SCLUI_FOLDER_PATH
+			+ "/temp.jpg";
+
+	/** 裁剪后保存的图片的文件名称 */
+	public static final String SAVE_IMAGE_NAME = "scl_" + TimeUtils.nowTime()
+			+ ".jpg";
+
+	/** 下载的图片的储存路径 */
+	public static final String DOWNLOAD_IMAGE_NAME = DOWNLOAD_FOLDER_PATH
+			+ "head_pic.jpg";
+
+	/** 下载的个人头像的储存路径 */
+	public static final String TEMP_IMAGE_NAME = DOWNLOAD_FOLDER_PATH
+			+ "temp.jpg";
+
+	/** 数字密码 */
+	public static final String PASSWORD = "scl_login_password";
+	/** 颜色值 */
+	public static final String COLOR = "scl_color";
 }
